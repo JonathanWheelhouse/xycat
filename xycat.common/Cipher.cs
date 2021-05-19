66634ae2e06d772ad8b62d5ce8715fbb33d11eba
Jonathan Wheelhouse <jonathan.wheelhouse@asx.com.au>
2021-05-19 10:17:36 +1000

    Whitespace fix

--- xycat.common/Cipher.cs
+++ xycat.common/Cipher.cs
@@ -10,16 +10,16 @@ namespace xycat.common
             var array = value
                 .ToCharArray()
                 .Select(c => c switch
-               {
-                   >= 'a' and <= 'z' when c  > 'm' => Convert.ToChar(c - 13),
-                   >= 'a' and <= 'z' when c <= 'm' => Convert.ToChar(c + 13),
+                {
+                    >= 'a' and <= 'z' when c  > 'm' => Convert.ToChar(c - 13),
+                    >= 'a' and <= 'z' when c <= 'm' => Convert.ToChar(c + 13),
 
-                   >= 'A' and <= 'Z' when c  > 'M' => Convert.ToChar(c - 13),
-                   >= 'A' and <= 'Z' when c <= 'M' => Convert.ToChar(c + 13),
+                    >= 'A' and <= 'Z' when c  > 'M' => Convert.ToChar(c - 13),
+                    >= 'A' and <= 'Z' when c <= 'M' => Convert.ToChar(c + 13),
 
-                   _ => Convert.ToChar(c)
-               })
-               .ToArray();
+                    _ => Convert.ToChar(c)
+                })
+                .ToArray();
 
             return new string(array);
         }
